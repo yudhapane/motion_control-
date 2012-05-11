e903c352465c4affdaadedf4cea67fb1fb67897e
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2012-05-11 13:16:37 +0200

    add support for force threshold events

--- cartesian_motion_control/src/CartesianImpedanceController.hpp
+++ cartesian_motion_control/src/CartesianImpedanceController.hpp
@@ -1,6 +1,6 @@
 /**************************************************************************
- *                (C) 2011 Ruben Smits                                    *
- *                ruben.smits@mech.kuleuven.be                            *
+ *         (C) 2011 Ruben Smits <ruben.smits@mech.kuleuven.be>            *
+ *  (C) 2012 Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>   *
  *               Department of Mechanical Engineering,                    *
  *              Katholieke Universiteit Leuven, Belgium.                  *
  *                                                                        *
@@ -43,24 +43,37 @@
 #include <tf_conversions/tf_kdl.h>
 #include <kdl/frames.hpp>
 
+using namespace RTT;
+
 namespace MotionControl{
 
 
     class CartesianImpedanceController : public RTT::TaskContext
     {
     public:
-        CartesianImpedanceController(const std::string& name): RTT::TaskContext(name){
+        CartesianImpedanceController(const std::string& name): 
+            RTT::TaskContext(name),
+            force_thres_exceeded(false)
+        {
             this->addEventPort("CartesianSensorPosition",port_pose_meas);
             this->addEventPort("CartesianDesiredPosition",port_pose_desi);
             this->addEventPort("CartesianSensorTwist",port_twist_meas);
             this->addEventPort("CartesianDesiredTwist",port_twist_desi);
 
             this->addPort("CartesianOutputWrench",port_wrench_out);
+            this->addPort("ext_ref_mode", ext_ref_mode)
+		    .doc("If 0, use mesured as desired pos, otherwise desired pos");
+
+            this->addPort("force_thres_ex", force_thres_ex).doc("force threshold exceeded event");
 
             this->addProperty("stiffness",m_stiffness).doc("Cartesian Stiffness in tool frame");
             this->addProperty("damping",m_damping).doc("Cartesian Damping in tool frame");
+
+            force_thres.resize(2);
+            this->addProperty("force_thres",force_thres).
+                doc("Force threshold for raising force_thres_exceeded event [trans,rot]");
         };
-        
+
     private:
         bool startHook(){
             if(port_pose_meas.read(m_pose_desi) == RTT::NoData )
@@ -72,9 +85,22 @@ namespace MotionControl{
             port_twist_desi.clear();
             return true;
         };
+
         void updateHook(){
-            KDL::Frame pm,pd;
-            KDL::Twist tm,td;
+            double norm_force, norm_torque;
+            KDL::Frame pm, pd;
+            KDL::Twist tm, td, delta_x, delta_x_dot;
+            RTT::FlowStatus fs;
+
+            bool cur_ext_ref_mode=false;
+
+            fs=ext_ref_mode.read(cur_ext_ref_mode);
+
+            if (cur_ext_ref_mode == false) {
+                m_wrench_out.force.x=0; m_wrench_out.force.y=0; m_wrench_out.force.z=0;
+                m_wrench_out.torque.x=0; m_wrench_out.torque.y=0; m_wrench_out.torque.z=0;
+                goto out_write;
+            }
 
             port_pose_meas.read(m_pose_meas);
             port_pose_desi.read(m_pose_desi);
@@ -85,17 +111,42 @@ namespace MotionControl{
             tf::PoseMsgToKDL(m_pose_desi,pd);
             tf::TwistMsgToKDL(m_twist_meas,tm);
             tf::TwistMsgToKDL(m_twist_desi,td);
-            
-            KDL::Twist delta_x = diff(pm.Inverse(),pd.Inverse());
-            KDL::Twist delta_x_dot = diff(tm, td);
+
+            delta_x = diff(pm.Inverse(),pd.Inverse());
+            delta_x_dot = diff(tm, td);
 
             m_wrench_out.force.x = m_stiffness.linear.x*delta_x.vel.x() + m_damping.linear.x*delta_x_dot.vel.x();
             m_wrench_out.force.y = m_stiffness.linear.y*delta_x.vel.y() + m_damping.linear.y*delta_x_dot.vel.y();
             m_wrench_out.force.z = m_stiffness.linear.z*delta_x.vel.z() + m_damping.linear.z*delta_x_dot.vel.z();
-            m_wrench_out.torque.x = m_stiffness.angular.x*delta_x.rot.x() + m_damping.angular.x*delta_x_dot.rot.x();;
-            m_wrench_out.torque.y = m_stiffness.angular.y*delta_x.rot.y() + m_damping.angular.y*delta_x_dot.rot.y();;
-            m_wrench_out.torque.z = m_stiffness.angular.z*delta_x.rot.z() + m_damping.angular.z*delta_x_dot.rot.z();;
+            m_wrench_out.torque.x = m_stiffness.angular.x*delta_x.rot.x() + m_damping.angular.x*delta_x_dot.rot.x();
+            m_wrench_out.torque.y = m_stiffness.angular.y*delta_x.rot.y() + m_damping.angular.y*delta_x_dot.rot.y();
+            m_wrench_out.torque.z = m_stiffness.angular.z*delta_x.rot.z() + m_damping.angular.z*delta_x_dot.rot.z();
 
+            // check threshold
+            norm_force = sqrt(pow(m_wrench_out.force.x, 2) +
+                              pow(m_wrench_out.force.y, 2) +
+                              pow(m_wrench_out.force.z, 2));
+
+            norm_torque = sqrt(pow(m_wrench_out.torque.x, 2) +
+                               pow(m_wrench_out.torque.y, 2) +
+                               pow(m_wrench_out.torque.z, 2));
+
+            // exceeding...
+            if(!force_thres_exceeded && (norm_force>force_thres[0] || norm_torque>force_thres[1])) {
+                force_thres_ex.write(true);
+                force_thres_exceeded=true;
+                log(Warning) << "exceeded for threshold, norm_force: " << norm_force
+                             << " , norm_torque: " << norm_torque << endlog();
+            }
+
+            if(force_thres_exceeded && (norm_force<force_thres[0] && norm_torque<force_thres[1])) {
+                force_thres_ex.write(false);
+                force_thres_exceeded=false;
+                log(Warning) << "fallen below threshold, norm_force: " << norm_force
+                             << " , norm_torque: " << norm_torque << endlog();
+            }
+
+	out_write:
             port_wrench_out.write(m_wrench_out);
         };
         void stopHook(){
@@ -117,7 +168,10 @@ namespace MotionControl{
         RTT::InputPort<geometry_msgs::Twist> port_twist_meas, port_twist_desi;
         RTT::OutputPort<geometry_msgs::Wrench> port_wrench_out;
 
-        
+        RTT::InputPort<bool> ext_ref_mode;
+        RTT::OutputPort<bool> force_thres_ex;
+        std::vector<double> force_thres;
+	    bool force_thres_exceeded;
     };
 }//namespace
         
